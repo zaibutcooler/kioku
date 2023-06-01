c1034f9a5363713a3ebaab88e7963613c792b529
Zai <zaiyellyintaung@gmail.com>
2023-06-01 16:13:36 +0630

    Before exam

--- frontend-react/src/pages/TodolistPage.jsx
+++ frontend-react/src/pages/TodolistPage.jsx
@@ -1,10 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Todo from "../components/Todo";
+import axios from "axios";
 
 function TodoList() {
+  const [datas, setDatas] = useState();
+  useEffect(() => {
+    // axios
+    //   .get("http://localhost:5000/todolist/")
+    //   .then((res) => setDatas(res.data))
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
+
+    fetch("http://localhost:5000/todolist/")
+      .then((res) => res.json)
+      .then((data) => setDatas(data));
+  }, []);
+
   return (
     <>
-      <Todo />
+      <div>
+        {datas &&
+          datas.map((data) => (
+            <div key={data._id} className="ml-4">
+              {data.title}
+            </div>
+          ))}
+      </div>
     </>
   );
 }
