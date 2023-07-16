94a0e044de9960d7f29ee814f5a104df8239e4bb
Zai <zaiyellyintaung@gmail.com>
2023-07-16 23:54:49 +0630

    done protected route

---
+++ backend/src/middlewares/protectRoute.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+const secretKey = String(process.env.SECRET_KEY);
+
+interface DecodedToken {
+  userID: string;
+}
+
+const protectRoutes = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
+      return res
+        .status(401)
+        .json({ message: "Authentication failed. Token not provided." });
+    }
+
+    const decoded = jwt.verify(token, secretKey) as DecodedToken;
+    req.userID = decoded.userID;
+    next();
+  } catch (err) {
+    return res
+      .status(401)
+      .json({ message: "Authentication failed. Invalid token." });
+  }
+};
