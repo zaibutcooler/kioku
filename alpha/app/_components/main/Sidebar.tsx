4040a3b4d4a2c37d87a475f6dbcee3491eb68f07
Zai <zaiyellyintaung@gmail.com>
2023-07-18 04:09:17 +0630

    Done not full sidebar

--- alpha/app/_components/main/Sidebar.tsx
+++ alpha/app/_components/main/Sidebar.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 import { useState } from "react";
 import {
   AiOutlineHome,
@@ -7,7 +8,12 @@ import {
   AiOutlineLogout,
   AiOutlineMan,
   AiOutlineAccountBook,
+  AiOutlineGroup,
+  AiOutlineAim,
+  AiOutlineDashboard,
 } from "react-icons/ai";
+import { FiLogOut, FiUser, FiUsers } from "react-icons/fi";
+
 const Sidebar = () => {
   const [isFull, setIsFull] = useState(false);
 
@@ -16,26 +22,36 @@ const Sidebar = () => {
       <section className="flex">
         <button
           className={
-            "w-12 h-12 hidden md:block absolute left-0 top-0 bg-transparent focus:outline-none "
+            "w-12 h-12 hidden md:block absolute left-0 top-0 bg-transparent focus:outline-none"
           }
           onClick={() => setIsFull(!isFull)}></button>
         <div className="w-2/3">
           {isFull ? (
             <div className="w-48 lg:w-52">Full</div>
           ) : (
-            <section className="  flex flex-col justify-center">
-              <div className="w-12 text-center text-lg">
+            <section className="w-14 px-1 flex flex-col items-center">
+              <Link
+                href="/home"
+                className="flex flex-col items-center justify-center hover:bg-gray-300 w-full py-2 mt-3 rounded-lg">
                 <AiOutlineHome />
-              </div>
-              <div className="w-12">
-                <AiOutlineAccountBook />
-              </div>
-              <div>
-                <AiOutlineProject />
-              </div>
-              <div>
-                <AiOutlineLogout />
-              </div>
+                <span className="text-[0.6rem] font-bold mt-0.5">Home</span>
+              </Link>
+              <Link
+                href="/home"
+                className="flex flex-col items-center justify-center hover:bg-gray-300 w-full py-2 mt-3 rounded-lg">
+                <FiUser />
+                <span className="text-[0.6rem] font-bold mt-0.5">Personal</span>
+              </Link>
+              <Link
+                href="/home"
+                className="flex flex-col items-center justify-center hover:bg-gray-300 w-full py-2 mt-3 rounded-lg">
+                <FiUsers />
+                <span className="text-[0.6rem] font-bold mt-0.5">Connect</span>
+              </Link>
+              <button className="flex flex-col items-center justify-center hover:bg-gray-300 w-full py-2 mt-3 rounded-lg">
+                <FiLogOut />
+                <span className="text-[0.6rem] font-bold mt-0.5">Logout</span>
+              </button>
             </section>
           )}
         </div>
