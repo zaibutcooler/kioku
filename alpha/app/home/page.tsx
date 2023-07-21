cbadcc7c81a11894e683fbff19c626f7076d79b9
Zai <zaiyellyintaung@gmail.com>
2023-07-21 15:52:30 +0630

    done status create form

--- alpha/app/home/page.tsx
+++ alpha/app/home/page.tsx
@@ -1,3 +1,11 @@
+import StatusCreateForm from "../_components/personal/status/StatusCreateForm";
+import GoalCreateForm from "../_components/personal/goal/GoalCreateForm";
+import TrackActionCreateForm from "../_components/personal/track/TrackActionCreateForm";
+
 export default function HomePage() {
-  return <main>Home Page</main>;
+  return (
+    <main>
+      <StatusCreateForm />
+    </main>
+  );
 }
