f8ec83dde769e594c54a2b64ec99ac023551b472
Zai <zaiyellyintaung@gmail.com>
2023-07-27 16:29:49 +0630

    making folder success

--- alpha/components/personal/note/NoteCreateForm.tsx
+++ alpha/components/personal/note/NoteCreateForm.tsx
@@ -1,15 +1,45 @@
-import React from "react";
+"use client";
+
+import { useEffect, useState } from "react";
 import { AiOutlineClose } from "react-icons/ai";
+import NoteFolderCreateForm from "./NoteFolderCreateForm";
+import { FaFolderPlus } from "react-icons/fa";
 
 interface Props {
   handleBack: () => void;
 }
 
 const NoteCreateForm: React.FC<Props> = ({ handleBack }) => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [related, setRelated] = useState("");
+
+  const userID = "64c16d804043c533448db52e";
+
+  const [newFolderName, setNewFolderName] = useState("");
+  const [currentFolder, setCurrentFolder] = useState("");
+  const [showFolderForm, setShowFolderForm] = useState(false);
+
+  const [folders, setFolders] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fillDatas = async () => {
+      const responseOne = await fetch(`/api/note/folder?userID=${userID}`);
+      const folders: any[] = await responseOne.json();
+      setFolders(folders);
+
+      const responseTwo = await fetch(`/api/note?userID=${userID}`);
+    };
+    fillDatas();
+  }, []);
+
   return (
     <main className="fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center bg-gray-200 bg-opacity-50 backdrop-filter backdrop-blur z-50 px-2">
+      {showFolderForm && (
+        <NoteFolderCreateForm handleBack={() => setShowFolderForm(false)} />
+      )}
       <div className="mx-auto">
-        <div className="bg-white shadow-md rounded-md py-4 w-full md:w-[600px] lg:w-[750px] text-xs md:text-sm font-normal">
+        <div className="bg-white shadow-md rounded-md py-4 w-full md:w-[600px] lg:w-[120vh] text-xs md:text-sm font-normal">
           <div className="h-[40px] px-8 flex border-b border-gray-100 justify-between items-top">
             <span className="font-semibold">Caputure Your Thoughts</span>
             <button onClick={handleBack}>
@@ -19,17 +49,17 @@ const NoteCreateForm: React.FC<Props> = ({ handleBack }) => {
           <section className="flex">
             <form className="bg-bg_white space-y-4 px-8 py-3 h-[75vh] overflow-y-auto scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-100 w-3/4">
               <div>
-                <label
-                  htmlFor="title"
-                  className="block text-xs font-medium text-gray-700">
+                <label className="block text-xs font-medium text-gray-700">
                   Title
                 </label>
                 <input
                   type="text"
                   id="title"
                   name="title"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
                   required
-                  className="mt-1 focus:ring-gray-400 focus:border-gray-400 block w-full text-xs border-gray-300 rounded-md p-2"
+                  className="mt-1 border focus:ring-gray-400 focus:border-gray-400 block w-full text-xs border-gray-200 rounded-sm p-2"
                   placeholder="Your Title"
                 />
               </div>
@@ -39,8 +69,10 @@ const NoteCreateForm: React.FC<Props> = ({ handleBack }) => {
                   id="description"
                   name="description"
                   required
+                  value={content}
+                  onChange={(e) => setContent(e.target.value)}
                   rows={4}
-                  className="mt-1 focus:ring-gray-400 focus:border-gray-400 block w-full text-xs border-gray-300 rounded-md p-2"
+                  className="mt-1 border focus:ring-gray-400 focus:border-gray-400 block w-full text-xs border-gray-200 rounded-sm p-2"
                   placeholder="Your Description"
                 />
               </div>
@@ -51,8 +83,20 @@ const NoteCreateForm: React.FC<Props> = ({ handleBack }) => {
                 Done
               </button>
             </form>
-            <div className="p-3 border bg-gray-50 w-1/4">
-              <main className="w-full h-full bg-white"></main>
+            <div className="py-3 bg-gray-50 w-1/4">
+              <main className="w-full h-full bg-white">
+                <div className=" px-3 flex justify-between items-center pb-3 ">
+                  <h1 className="font-semibold text-sm">Folders</h1>
+                  <button
+                    onClick={() => setShowFolderForm(true)}
+                    className="p-1 border rounded-sm text-xs">
+                    <FaFolderPlus />
+                  </button>
+                </div>
+                <div className="h-full pl-4 pr-3">
+                  {folders && folders.map((item) => <div>{item.name}</div>)}
+                </div>
+              </main>
             </div>
           </section>
         </div>
---
+++ alpha/components/personal/note/NoteFolderCreateForm.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { useState } from "react";
+import { AiOutlineClose } from "react-icons/ai";
+
+interface Props {
+  handleBack: () => void;
+}
+
+const NoteFolderCreateForm: React.FC<Props> = ({ handleBack }) => {
+  const [name, setName] = useState("");
+
+  const userID = "64c16d804043c533448db52e";
+
+  const handleSubmit = async () => {
+    const response = await fetch(`/api/note/folder`, {
+      method: "POST",
+      body: JSON.stringify({
+        user: userID,
+        name: name,
+      }),
+    });
+    if (response.ok) {
+      handleBack();
+    }
+  };
+
+  return (
+    <main className="fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center bg-gray-300 bg-opacity-50 backdrop-filter backdrop-blur z-50 px-2">
+      <div className="mx-auto">
+        <div className="bg-white shadow-md rounded-md py-4 w-3/4 md:w-[260px] lg:w-[300px]  text-xs md:text-sm font-normal px-4">
+          <label className="block text-sm font-semibold text-gray-700 py-1 mb-4">
+            Create New Folder
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+            className="mt-1 border  block w-full text-xs border-gray-200 rounded-sm p-2"
+            placeholder="Folder Name"
+          />
+          <div className="w-full mt-4 flex justify-between text-xs items-center">
+            <button className="font-semibold px-1" onClick={handleBack}>
+              Back
+            </button>
+            <button
+              className="px-3 py-1.5 bg-black text-white rounded-sm"
+              onClick={handleSubmit}>
+              Create
+            </button>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default NoteFolderCreateForm;
