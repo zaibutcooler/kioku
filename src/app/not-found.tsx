9dce142aee24a39c44e5447c371eaf6dacb9d66f
Zai <130903099+zaiYellYintAung@users.noreply.github.com>
2024-03-30 23:59:40 +0630

    dashboard layout

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,42 +1,11 @@
 import Link from "next/link"
 
 import { buttonVariants } from "@/components/ui/button"
-import { LandingFooter } from "@/components/navigations/landing/landing-footer"
-import { LandingNavbar } from "@/components/navigations/landing/landing-navbar"
-
 export default function NotFoundPage() {
   return (
     <div>
-      <div className="h-screen">
-        <LandingNavbar />
-        <div className="flex h-full w-full items-center justify-center">
-          <div className="flex items-center justify-center">
-            <main className="grid min-h-full place-items-center bg-background ">
-              <div className="text-center">
-                <p className="text-base font-semibold text-primary">404</p>
-                <h1 className="mt-4 text-3xl font-bold tracking-tight text-foreground sm:text-5xl">
-                  Page not found
-                </h1>
-                <p className="mt-6 text-base leading-7 text-muted-foreground">
-                  Sorry, we couldn’t find the page you’re looking for.
-                </p>
-                <div className="mt-10 flex items-center justify-center gap-x-6">
-                  <Link href="/" className={buttonVariants({})}>
-                    Go back home
-                  </Link>
-                  <Link
-                    href="#"
-                    className={buttonVariants({ variant: "ghost" })}
-                  >
-                    Contact support <span aria-hidden="true">&rarr;</span>
-                  </Link>
-                </div>
-              </div>
-            </main>
-          </div>
-        </div>
-      </div>
-      <LandingFooter />
+      <div className="h-screen">Not found</div>
+
     </div>
   )
 }
--- src/env.js
+++ src/env.js
@@ -1,5 +1,4 @@
 import { createEnv } from "@t3-oss/env-nextjs";
-import { z } from "zod";
 
 export const env = createEnv({
   /**
@@ -7,29 +6,29 @@ export const env = createEnv({
    * isn't built with invalid env vars.
    */
   server: {
-    DATABASE_URL: z
-      .string()
-      .url()
-      .refine(
-        (str) => !str.includes("YOUR_MYSQL_URL_HERE"),
-        "You forgot to change the default URL"
-      ),
-    NODE_ENV: z
-      .enum(["development", "test", "production"])
-      .default("development"),
-    NEXTAUTH_SECRET:
-      process.env.NODE_ENV === "production"
-        ? z.string()
-        : z.string().optional(),
-    NEXTAUTH_URL: z.preprocess(
-      // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
-      // Since NextAuth.js automatically uses the VERCEL_URL if present.
-      (str) => process.env.VERCEL_URL ?? str,
-      // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-      process.env.VERCEL ? z.string() : z.string().url()
-    ),
-    DISCORD_CLIENT_ID: z.string(),
-    DISCORD_CLIENT_SECRET: z.string(),
+    // DATABASE_URL: z
+    //   .string()
+    //   .url()
+    //   .refine(
+    //     (str) => !str.includes("YOUR_MYSQL_URL_HERE"),
+    //     "You forgot to change the default URL"
+    //   ),
+    // NODE_ENV: z
+    //   .enum(["development", "test", "production"])
+    //   .default("development"),
+    // NEXTAUTH_SECRET:
+    //   process.env.NODE_ENV === "production"
+    //     ? z.string()
+    //     : z.string().optional(),
+    // NEXTAUTH_URL: z.preprocess(
+    //   // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
+    //   // Since NextAuth.js automatically uses the VERCEL_URL if present.
+    //   (str) => process.env.VERCEL_URL ?? str,
+    //   // VERCEL_URL doesn't include `https` so it cant be validated as a URL
+    //   process.env.VERCEL ? z.string() : z.string().url()
+    // ),
+    // DISCORD_CLIENT_ID: z.string(),
+    // DISCORD_CLIENT_SECRET: z.string(),
   },
 
   /**
